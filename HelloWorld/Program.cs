40ec419229ee380c0d7b2a193340be8b895e1618
Donovan Murphy <dmurphy@lmcintranet.org>
2019-05-29 16:17:32 -0500

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Arrays
+            //School Tracker
+            var studentGrades = new int[10] {80, 77, 45, 87, 53, 43, 67, 34, 23, 76};
+
+            for (int i = 0; i < 10; i++)
+            {
+                var ctStudent = i + 1;
+                Console.WriteLine("Student {0}'s Grade is {1}", ctStudent.ToString(), studentGrades[i].ToString());
+            }
+            
+        }
+    }
+}
